f9bf965c8ba9f442f5fc3a1e21a257e6056b9964
Rui Nan <nanruihit@gmail.com>
2021-08-22 14:21:43 -0700

    save

--- src/com/rui/FindMaxiumValueInAStreamInLastXSeconds.java
+++ src/com/rui/FindMaxiumValueInAStreamInLastXSeconds.java
@@ -1,5 +1,9 @@
 package com.rui;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class FindMaxiumValueInAStreamInLastXSeconds {
     
 //    *Given a streaming data of the form (timestamp, value), find the maximum value in the stream in the last X seconds.*
@@ -9,7 +13,7 @@ public class FindMaxiumValueInAStreamInLastXSeconds {
 //            *max_value() function finds the max in the last X seconds.*
 //
 //            *StreamProcessor(5) // last 5 seconds*
-//*set_value(0, 5)*
+//            *set_value(0, 5)*
 //            *set_value(1, 6)*
 //            *set_value(2, 4)*
 //            *max_value(3) = 6 -> always the current time*
@@ -23,4 +27,41 @@ public class FindMaxiumValueInAStreamInLastXSeconds {
 //
 //            *def max_value(self, cur_t):*
 //              *pass*
+    class Data {
+        int time;
+        int value;
+        public Data(int time, int value) {
+            this.time = time;
+            this.value = value;
+        }
+    }
+
+    class StreamProcessor {
+        // 题目只说了时间上是单调递增的，没有说时间是连续的或者是间隔一定在X 之内。所以我觉得可以用单调队列。
+        Deque<Data> queue;
+        int X;
+        public StreamProcessor (int X) {
+            // 单调递减
+            queue = new ArrayDeque<>();
+            this.X = X;
+        }
+
+        void setValue(int time, int value) {
+            while (!queue.isEmpty() && time - queue.peekFirst().time >= this.X) {
+                queue.pollFirst();
+            }
+            while (!queue.isEmpty() && value > queue.peekLast().value) {
+                queue.pollLast();
+            }
+            queue.offer(new Data(time, value));
+        }
+
+        int maxValue() throws Exception {
+            if (queue.isEmpty()) {
+                throw new Exception("no data");
+            }
+            return queue.peekFirst().value;
+        }
+
+    }
 }
