9b1415d8465f3a4248c68167692af596d93d23d5
Rui Nan <nanruihit@gmail.com>
2021-09-09 23:36:15 -0700

    save

--- src/com/rui/KAnagram.java
+++ src/com/rui/KAnagram.java
@@ -32,4 +32,21 @@ public class KAnagram {
         }
         return diff <= k;
     }
+
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {
+            return false;
+        }
+        int[] countS = new int[26], countT = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            countS[s.charAt(i) - 'a']++;
+            countT[t.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (countS[i] != countT[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
